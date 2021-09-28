313c23f2e2b79c9407cd2482f06eb4acd5b0121a
Aman Siddiqui <amansiddiquias123@gmail.com>
2021-09-28 19:20:50 +0500

    Working on tmdb key and getting all urls needed for the application

--- netflix-clone/src/allurls.js
+++ netflix-clone/src/allurls.js
@@ -1,4 +1,4 @@
-const api_key = "0ac87fe9ef3c5a4ad3649cd43307fd9c"
+const api_key = "0ac87fe9ef3c5a4ad3649cd43307fd9c";
 
 const urls = {
     netflixOrignals:`/discover/tv?api_key=${api_key}&with_networks=213`,
