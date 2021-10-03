2f159ca8715813700e0921f8320a9ad53d6e18a7
Aman Siddiqui <amansiddiquias123@gmail.com>
2021-10-03 11:51:48 +0500

    Adding a home page for the website

---
+++ netflix-clone/src/components/Start.js
@@ -0,0 +1,46 @@
+import logo from '../assets/logo.png'
+import './Start.css'
+import { Fragment,useRef } from 'react'
+import {useSelector,useDispatch} from "react-redux"
+import { authActions } from '../store'
+import { useHistory } from 'react-router'
+
+const Start = ()=>{
+    const emailRef = useRef()
+    const dispatch = useDispatch()
+    const history = useHistory()
+    const submitHandler = ()=>{
+        const emailValue = emailRef.current.value
+        dispatch(authActions.emailDispatching(emailValue))
+        history.push("/auth")
+
+    }
+    return(
+        <Fragment>
+        <div className="start">
+            <nav>
+                <img  src= {logo} alt="Company log" />
+                <button>Sign in</button>
+                
+            </nav>
+            <div className ="startmid">
+                <h2>Unlimited movies,TV <br/>shows, and more.</h2>
+                <h5>Watch anywhere. Cancel anytime.</h5>
+                <p>Ready to watch? Enter your email to create or restart your membership.</p>
+               
+                    <form onSubmit={submitHandler} className="last">
+                <input type="email"  ref={emailRef} required/>
+                <button type="submit">Get Started  </button>
+                </form>
+                
+
+            </div>
+            <div className="lastFading"></div>
+            </div>
+          
+</Fragment>
+
+    )
+}
+
+export default Start
